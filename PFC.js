e89da60e279fa1009b065382692abd2e0009581a
Léo Ferron <leoferron10@gmail.com>
2024-05-29 22:06:42 +0200

    ajout du html css et reecriture du js

--- PFC.js
+++ PFC.js
@@ -1,30 +1,56 @@
+let wins =0;
+let loses=0;
+const choices =["papier","caillou","ciseaux"];
+const gamesStatus = document.getElementById("gameStatus")
+const gamesScore = document.getElementById("gameScore")
+const gameResult = document.getElementById("gameResult")
+const paper = document.getElementById("paper")
+const rock = document.getElementById("rock")
+const scissors = document.getElementById("scissors")
 
-let ordi = ['P','F','C'];
-let scoreJ = 0
-let scoreO = 0
+paper.addEventListener("click",()=>runGame("papier"))
+rock.addEventListener("click",()=>runGame("caillou"))
+scissors.addEventListener("click",()=>runGame("ciseaux"))
 
-while(scoreJ != 2 && scoreO != 2){
-let joueur = prompt('pierre(P), feuille(F), ciseaux(C)??');
-let rOrdi = Math.floor(Math.random() * ordi.length);
-let rValue = ordi[rOrdi];
-let resultat = "";
-alert(rValue)
-if(joueur === rValue) {
-    resultat = "égalité";
-    alert(resultat);
-} else if((joueur === "P" && rValue === "C") || (joueur === "F" && rValue === "P") || (joueur === "C" && rValue === "F")) {
-    resultat = "manche gagnée";
-    scoreJ++;
-    alert(resultat);
-} else {
-    resultat = "manche perdue";
-    scoreO++;
-    alert(resultat);
+function runGame(userchoice){
+    const computerChoice = choices[Math.floor(Math.random()*choices.length)]
+switch(userchoice +'_'+ computerChoice){
+    case 'papier_ciseaux':
+    case 'caillou_papier':
+    case 'ciseaux_caillou':
+            loses++;
+            gamesStatus.innerHTML= `Moi: ${userchoice} | Ordi: ${computerChoice}`
+            gameResult.innerHTML = `Perdu!`
+      break;
+        case 'papier_caillou':
+        case 'caillou_ciseaux':
+        case 'ciseaux_papier':
+                wins++;
+                gamesStatus.innerHTML= `Moi: ${userchoice} | Ordi: ${computerChoice}`
+                gameResult.innerHTML = `Gagné!`
+        break;
+            case 'papier_papier':
+            case 'caillou_caillou':
+            case 'ciseaux_ciseaux':
+                    gamesStatus.innerHTML= `Moi: ${userchoice} | Ordi: ${computerChoice}`
+                    gameResult.innerHTML = `Egalité!`
+            break;
+            
+    }
+    gamesScore.innerHTML = `Moi: ${wins} | Ordi: ${loses}`
+    if (wins === 2) {
+        gamesStatus.innerHTML = 'Tu as gagné la partie';
+        setTimeout(resetGame, 2000); 
+    }
+    if (loses === 2) {
+        gamesStatus.innerHTML = 'Tu as perdu la partie';
+        setTimeout(resetGame, 2000); 
+    }
 }
-
-}
-if(scoreJ === 2) {
-alert("Vous avez gagné la partie !");
-} else {
-alert("L'ordinateur a gagné la partie !");
+function resetGame() {
+    wins = 0;
+    loses = 0;
+    gamesStatus.innerHTML = 'Selctionnez votre arme!';
+    gameResult.innerHTML = ''
+    gamesScore.innerHTML = `Moi: ${wins} | Ordi: ${loses}`;
 }
